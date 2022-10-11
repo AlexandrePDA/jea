aadd1b1ec391e5da29ffb9749744ad4f0c1b3701
AlexandrePDA <a.pires130@gmail.com>
2022-10-11 02:41:41 +0200

    integration of header & first section 🚀

--- src/Components/Accueil.jsx
+++ src/Components/Accueil.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import ordi from '../assets/login.png'
 
 const Accueil = () => {
     return (
-        <section>
+        <section className="w-screen flex flex-col items-center justify-center lg:flex-row">
+            <h1 className="text-light-p"><span>Junior</span>, et alors ?</h1>
+           <img src={ordi} alt="ordinateur" className="max-width-sm"/>
         </section>
     );
 };
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -17,26 +17,26 @@ const Header = () => {
   return (
     <header className="h-16 w-full shadow-md ">
       <div className="mx-8 items-center justify-between md:flex">
-        <div className="cursor-pointer flex items-center  text-white">
+        <div className="cursor-pointer flex items-center  text-light-p">
           <img src={logo} alt="logo jea" className="h-16" />
           <p className="text-base ml-2 md:text-xl">JEA</p>
         </div>
         <div
           onClick={() => setBurger(!burger)}
-          className="text-3xl absolute right-8 top-4 text-white cursor-pointer md:hidden"
+          className="text-3xl absolute right-8 top-4 text-light-p cursor-pointer md:hidden"
         >
           <ion-icon name={burger ? "close-circle-sharp" : "grid-outline"}></ion-icon>
         </div>
 
-        <ul className={`bg-dark-p pb-12 pl-12 pt-12 absolute text-center z-[-1] w-full left-0 transition-all duration-300 ease-in ${
+        <ul className={`bg-dark-p text-light-p pb-12 pl-12 pt-12 absolute text-center z-[-1] w-full left-0 transition-all duration-300 ease-in ${
             burger ? "top-10 opacity-100" : "top-[-490px]"
           } opacitiy-0 md:opacity-100 md:z-auto md:w-auto md:static md:flex md:items-center md:pt-0 md:pb-0 md:pl-0`}
         >
             
-            <li className="my-7 md:ml-8 md:my-0">
+            <li className="my-7 md:ml-8 md:my-0 ">
               <a
                 href="/ressources"
-                className="text-white flex items-center hover:bg-slate-blue duration-300"
+                className="flex items-center hover:text-slate-400 duration-300"
                 onClick={handleClick}
                 
               >
@@ -49,7 +49,7 @@ const Header = () => {
             <li className="my-7 md:ml-8 md:my-0">
               <a
                 href="/outils"
-                className="text-white flex items-center hover: duration-300"
+                className="flex items-center hover:text-slate-400 duration-300"
                 onClick={handleClick}
                 
               >
@@ -58,10 +58,10 @@ const Header = () => {
               </a>
             </li>
 
-            <li className="my-7 w-1/3 md:ml-8 md:my-0 bg-gradient-to-r from-pink-p to-blue-p py-2 px-4 rounded-xl">
+            <li className="my-7 w-1/3 md:ml-8 md:my-0 bg-gradient-to-r from-pink-p to-blue-p py-2 px-4 rounded-xl hover:text-slate-300">
               <a
                 href="/blog"
-                className="text-white flex items-center hover: duration-300"
+                className="flex items-center hover: duration-300"
                 onClick={handleClick}
                 
               >
